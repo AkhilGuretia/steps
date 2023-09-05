01680133aa2c95a9d972ebbb2b1ac5cd003d2493
Akhil Kumar <akhilguretia@gmail.com>
2023-09-06 00:35:34 +0530

    update the App with static changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import "./index.css";
 
-
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
 
 export default function App() {
+  const step = 3;
+
   return (
-    <div>
-      Hello React!
+    <div className="steps">
+
+      <div className="numbers">
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
+      </div>
+
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
+
+      <div className="buttons">
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          className="previous" >
+          previous
+        </button>
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          className="next" >
+          next
+        </button>
+      </div>
+
     </div>
   );
 }
